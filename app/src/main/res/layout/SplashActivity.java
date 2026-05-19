f577022140e05757100f334aedf82c1fdb871773
anggas <anggasspm@gmail.com>
2026-05-19 09:03:02 +0700

    Add SplashActivity for app launch screen

---
+++ app/src/main/res/layout/SplashActivity.java
@@ -0,0 +1,12 @@
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, LoginOrSignUpActivity.class));
+            finish();
+        }, 2000);
+    }
+}
