5b90a0a155d39aaee1647421cedc03263e3c34c1
Saif Ali <saifalialhaddad2@gmail.com>
2026-05-25 23:45:06 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,11 @@ android {
 }
 
 dependencies {
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // OkHttp Logging (untuk debug)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     // ✅ Kotlin DSL: double quotes, parentheses, no semicolons
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
