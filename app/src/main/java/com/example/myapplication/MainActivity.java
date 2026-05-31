e442d951fe46eea9f5fe304f5fba0295a297c438
saifalihdd <saifalialhaddad2@gmail.com>
2026-05-31 11:45:53 +0700

    Update fragment imports in MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+// GANTI semua import fragment lama dengan ini:
 import com.example.myapplication.fragments.ChatFragment;
 import com.example.myapplication.fragments.HomeFragment;
 import com.example.myapplication.fragments.PostFragment;
